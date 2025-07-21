40236a776040ce246a77f9dba56dcc9877aa6ae6
Vasyl Kyrashchuk <47303372+Citizen4our@users.noreply.github.com>
2025-07-21 17:34:01 +0300

    Update src/Instrumentation/HookInstrumentationInterface.php
    Co-authored-by: Zakhar <zahar.guzenko@gmail.com>

--- src/Instrumentation/HookInstrumentationInterface.php
+++ src/Instrumentation/HookInstrumentationInterface.php
@@ -8,11 +8,15 @@ interface HookInstrumentationInterface extends InstrumentationInterface
 {
     /**
      * Hook class.
+     * 
+     * @return class-string|null
      */
     public function getClass(): ?string;
 
     /**
      * Hook method. If class is null, attached to predefined function.
+     * 
+     * @return non-empty-string
      */
     public function getMethod(): string;
 }

8f0563f86cf96b63b2cb3507dac753f2ddc6a493
Serhii Donii <serhii.donii@macpaw.com>
2025-12-09 01:05:47 +0200

    feat: orc-9153 add k6 tests
    Signed-off-by: Serhii Donii <serhii.donii@macpaw.com>

--- loadTesting/cqrs-test.js
+++ loadTesting/cqrs-test.js
@@ -10,7 +10,7 @@ import { BASE_URL } from './config.js';
 
 export const options = {
     vus: 10,
-    duration: '2m',
+    duration: '3s',
     thresholds: {
         http_req_duration: ['p(95)<500', 'p(99)<1000'],
         http_req_failed: ['rate<0.01'],
